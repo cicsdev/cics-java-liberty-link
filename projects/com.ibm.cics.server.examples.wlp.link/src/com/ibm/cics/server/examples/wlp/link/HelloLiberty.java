a97d592a10c24e32e9698843e8b531d04cb4e576
Matthew Wilson <matthew.wilson@uk.ibm.com>
2016-11-08 09:25:52 +0000

    Add projects

---
+++ projects/com.ibm.cics.server.examples.wlp.link/src/com/ibm/cics/server/examples/wlp/link/HelloLiberty.java
@@ -0,0 +1,46 @@
+package com.ibm.cics.server.examples.wlp.link;
+
+import com.ibm.cics.server.CCSIDErrorException;
+import com.ibm.cics.server.Channel;
+import com.ibm.cics.server.ChannelErrorException;
+import com.ibm.cics.server.CodePageErrorException;
+import com.ibm.cics.server.Container;
+import com.ibm.cics.server.ContainerErrorException;
+import com.ibm.cics.server.InvalidRequestException;
+import com.ibm.cics.server.Task;
+import com.ibm.cics.server.invocation.CICSProgram;
+
+/**
+ * Sample code to be invoked by EXEC CICS LINK.
+ */
+public class HelloLiberty {
+
+	/**
+	 * Prints a greeting message to STDOUT. When invoked with a text container
+	 * called NAME on the current channel. that name is used. Otherwise, the
+	 * user ID of the CICS task is used.
+	 */
+	@CICSProgram("HELLOWLP")
+	public void printMessage() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Hello ");
+		try {
+			Channel currentChannel = Task.getTask().getCurrentChannel();
+			if (currentChannel != null && currentChannel.getContainer("NAME") != null) {
+				Container nameContainer = currentChannel.getContainer("NAME");
+				sb.append(nameContainer.getString());
+			} else {
+				sb.append(Task.getTask().getUSERID());
+			}
+			sb.append(" from Liberty server ");
+			sb.append(System.getProperty("com.ibm.cics.jvmserver.wlp.server.name"));
+			System.out.println(sb.toString());
+		} catch (InvalidRequestException | ContainerErrorException
+				| ChannelErrorException | CCSIDErrorException
+				| CodePageErrorException e) {
+			e.printStackTrace();
+			Task.getTask().abend("OHNO");
+		}
+
+	}
+}
